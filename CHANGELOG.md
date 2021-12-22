e537290a8a97b6291d75247c8d0a497ba75612de
Jean-Francis Bastien <bhacaz@gmail.com>
2021-12-22 07:32:55 -0500

    Added RSpec/CreateListMax

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# Changelog
+
+# main
+
+* Added cop `RSpec/CreateListMax`
+
+# v0.1.0
+
+* First version
---
+++ spec/rubocop/cop/rspec/create_list_max_spec.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+RSpec.describe RuboCop::Cop::RSpec::CreateListMax, :config do
+  let(:cop_config) { RuboCop::Config.new('Max' => 2) }
+
+  it 'registers an offense when creating more than Max' do
+    expect_offense(<<~RUBY)
+      create_list :my_model, 100
+      ^^^^^^^^^^^^^^^^^^^^^^^^^^ Creating more than `2` records is discouraged.
+    RUBY
+
+    expect_offense(<<~RUBY)
+      my_records = create_list :my_model, 100
+                   ^^^^^^^^^^^^^^^^^^^^^^^^^^ Creating more than `2` records is discouraged.
+    RUBY
+
+    expect_offense(<<~RUBY)
+      FactoryBot.create_list :my_model, 100, name: 'Hello'
+      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Creating more than `2` records is discouraged.
+    RUBY
+
+    expect_offense(<<~RUBY)
+      FactoryBot.create_list(:my_model, 100, name: 'Hello')
+      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Creating more than `2` records is discouraged.
+    RUBY
+
+    expect_offense(<<~RUBY)
+      create_list :my_model, 100, name: 'Hello' do |my_record|
+      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Creating more than `2` records is discouraged.
+      end
+    RUBY
+  end
+
+  it 'does not register an offense when using less than Max' do
+    expect_no_offenses(<<~RUBY)
+      create_list :my_model, 2, name: 'Hello'
+    RUBY
+
+    expect_no_offenses(<<~RUBY)
+      build_list :my_model, 100, name: 'Hello'
+    RUBY
+  end
+end
