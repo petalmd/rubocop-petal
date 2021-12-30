36ed0a2162f555a1453294e95c4d6b5167d9ae86
Maxime Duchene-Savard <mduchenesavard@petalmd.com>
2021-12-30 12:53:58 -0500

    fix cop offenses + add test cases

--- spec/rubocop/cop/grape/prefer_namespace_spec.rb
+++ spec/rubocop/cop/grape/prefer_namespace_spec.rb
@@ -1,71 +1,108 @@
 # frozen_string_literal: true
 
 RSpec.describe RuboCop::Cop::Grape::PreferNamespace, :config do
-  it 'registers an offense when using one of `namespace` aliases' do
-    expect_offense(<<~RUBY)
-      group :my_group do
-      ^^^^^^^^^^^^^^^ Prefer using `namespace` over its aliases.
-        get {}
-      end
-    RUBY
+  context 'when using an alias' do
+    context 'when using `group`' do
+      it 'registers an offense and corrects' do
+        expect_offense(<<~RUBY)
+          group :my_group do
+          ^^^^^^^^^^^^^^^ Prefer using `namespace` over its aliases.
+            get {}
+          end
+        RUBY
 
-    expect_correction(<<~RUBY)
-      namespace :my_group do
-        get {}
+        expect_correction(<<~RUBY)
+          namespace :my_group do
+            get {}
+          end
+        RUBY
       end
-    RUBY
+    end
 
-    expect_offense(<<~RUBY)
-      resource :my_resource do
-      ^^^^^^^^^^^^^^^^^^^^^ Prefer using `namespace` over its aliases.
-        get {}
-      end
-    RUBY
+    context 'when using `resource`' do
+      it 'registers an offense and corrects' do
+        expect_offense(<<~RUBY)
+          resource :my_resource do
+          ^^^^^^^^^^^^^^^^^^^^^ Prefer using `namespace` over its aliases.
+            get {}
+          end
+        RUBY
 
-    expect_correction(<<~RUBY)
-      namespace :my_resource do
-        get {}
+        expect_correction(<<~RUBY)
+          namespace :my_resource do
+            get {}
+          end
+        RUBY
       end
-    RUBY
+    end
 
-    expect_offense(<<~RUBY)
-      resources :my_resources do
-      ^^^^^^^^^^^^^^^^^^^^^^^ Prefer using `namespace` over its aliases.
-        get {}
-      end
-    RUBY
+    context 'when using `resources`' do
+      it 'registers an offense and corrects' do
+        expect_offense(<<~RUBY)
+          resources :my_resources do
+          ^^^^^^^^^^^^^^^^^^^^^^^ Prefer using `namespace` over its aliases.
+            get {}
+          end
+        RUBY
 
-    expect_correction(<<~RUBY)
-      namespace :my_resources do
-        get {}
+        expect_correction(<<~RUBY)
+          namespace :my_resources do
+            get {}
+          end
+        RUBY
       end
-    RUBY
+    end
 
-    expect_offense(<<~RUBY)
-      segment :my_segment do
-      ^^^^^^^^^^^^^^^^^^^ Prefer using `namespace` over its aliases.
-        get {}
-      end
-    RUBY
+    context 'when using `segment`' do
+      it 'registers an offense and corrects' do
+        expect_offense(<<~RUBY)
+          segment :my_segment do
+          ^^^^^^^^^^^^^^^^^^^ Prefer using `namespace` over its aliases.
+            get {}
+          end
+        RUBY
 
-    expect_correction(<<~RUBY)
-      namespace :my_segment do
-        get {}
+        expect_correction(<<~RUBY)
+          namespace :my_segment do
+            get {}
+          end
+        RUBY
       end
-    RUBY
+    end
 
-    expect_offense(<<~RUBY)
-      segment :my_segment, 'some other param' do
-      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Prefer using `namespace` over its aliases.
-        get {}
-      end
-    RUBY
+    context 'when there are multiple parameters' do
+      it 'corrects without breaking the params' do
+        expect_offense(<<~RUBY)
+          segment :my_segment, 'some other param' do
+          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Prefer using `namespace` over its aliases.
+            get {}
+          end
+        RUBY
 
-    expect_correction(<<~RUBY)
-      namespace :my_segment, 'some other param' do
-        get {}
+        expect_correction(<<~RUBY)
+          namespace :my_segment, 'some other param' do
+            get {}
+          end
+        RUBY
       end
-    RUBY
+    end
+
+    context 'when the namespace is passed as a string' do
+      it 'corrects without breaking the params' do
+        expect_offense(<<~RUBY)
+          segment 'some_str', 'some other param' do
+          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Prefer using `namespace` over its aliases.
+            get {}
+          end
+        RUBY
+
+        expect_correction(<<~RUBY)
+          namespace 'some_str', 'some other param' do
+            get {}
+          end
+        RUBY
+      end
+    end
   end
 
   it 'does not register an offense when using namespace' do
