90512c2789ed263c67b8f5c9490858050a213aff
Jean-Francis Bastien <jfbastien@petalmd.com>
2022-01-21 15:09:48 -0500

    Add ForeignKeyOption cop (#11)
    * Add ForeignKeyOption cop
    
    * typo add_reference

---
+++ lib/rubocop/cop/migration/foreign_key_option.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+module RuboCop
+  module Cop
+    module Migration
+      # Specify the foreign key option to create the constraint.
+      #
+      #   # bad
+      #   add_reference(:products, :user)
+      #   add_reference(:products, :user, foreign_key: false)
+      #   add_belongs_to(:products, :user)
+      #   t.references(:user)
+      #
+      #   # good
+      #   add_reference(:products, :user, foreign_key: true)
+      #   add_reference(:products, :user, foreign_key: { to_table: users })
+      #   add_belongs_to(:products, :user, foreign_key: true)
+      #   t.references(:user, foreign_key: true)
+      #
+      class ForeignKeyOption < Base
+        MSG = 'Add `foreign_key: true` or `foreign_key: { to_table: :some_table }`'
+
+        ADDING_REFERENCES_METHODS = Set.new(%i[add_reference add_belongs_to references belongs_to]).freeze
+
+        def_node_matcher :adding_reference?, <<~PATTERN
+          (send _ ADDING_REFERENCES_METHODS ...)
+        PATTERN
+
+        def_node_matcher :foreign_key_option?, <<~PATTERN
+          (send _ ADDING_REFERENCES_METHODS ... (hash <(pair (sym :foreign_key) {true hash}) ...>))
+        PATTERN
+
+        def_node_matcher :polymorphic_reference?, <<~PATTERN
+          (... (hash <(pair (sym :polymorphic) true) ... >))
+        PATTERN
+
+        def on_send(node)
+          return unless adding_reference?(node)
+          return if polymorphic_reference?(node)
+
+          add_offense(node) unless foreign_key_option?(node)
+        end
+      end
+    end
+  end
+end
---
+++ spec/rubocop/cop/migration/foreign_key_option_spec.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+RSpec.describe RuboCop::Cop::Migration::ForeignKeyOption, :config do
+  it 'registers an offense when not specifying the foreign key option' do
+    expect_offense(<<~RUBY)
+      add_reference :products, :user
+      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Add `foreign_key: true` or `foreign_key: { to_table: :some_table }`
+
+      add_belongs_to :products, :user
+      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Add `foreign_key: true` or `foreign_key: { to_table: :some_table }`
+
+      t.references :user
+      ^^^^^^^^^^^^^^^^^^ Add `foreign_key: true` or `foreign_key: { to_table: :some_table }`
+
+      t.belongs_to :user
+      ^^^^^^^^^^^^^^^^^^ Add `foreign_key: true` or `foreign_key: { to_table: :some_table }`
+
+      add_reference :products, :user, index: true, null: false
+      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Add `foreign_key: true` or `foreign_key: { to_table: :some_table }`
+
+      add_reference :products, :user, foreign_key: false
+      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Add `foreign_key: true` or `foreign_key: { to_table: :some_table }`
+    RUBY
+  end
+
+  it 'does not register an offense when setting foreign_key option to create constrant' do
+    expect_no_offenses(<<~RUBY)
+      add_reference :products, :user, foreign_key: true
+
+      add_belongs_to :products, :user, foreign_key: true
+
+      t.references :user, foreign_key: true
+
+      t.belongs_to :user, foreign_key: true
+
+      add_reference :products, :user, foreign_key: { to_table: :users }
+
+      add_reference :products, :user, polymorphic: true
+
+      t.references :user, polymorphic: true
+    RUBY
+  end
+end
