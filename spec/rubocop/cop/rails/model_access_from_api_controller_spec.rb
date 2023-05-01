e940165072cf318b824249edaa3fea3ee674ca7a
Najib El Alam <nelalam@petalmd.com>
2023-05-01 09:33:05 -0400

    SEME-1955: New Cop - Do not directly access models from within Rails Api / Controller

---
+++ spec/rubocop/cop/rails/model_access_from_api_controller_spec.rb
@@ -0,0 +1,204 @@
+# frozen_string_literal: true
+
+RSpec.describe RuboCop::Cop::Rails::ModelAccessFromApiController, :config do
+  subject(:cop) { described_class.new(config) }
+
+  let(:config) do
+    RuboCop::Config.new(
+      'Rails/ModelAccessFromApiController' => {
+        'DisabledApis' => %w[
+          fake_disabled_api
+          FakeDisabledApiCamel
+        ], 'DisabledControllers' => %w[
+             fake_disabled_controller
+             FakeDisabledControllerCamel
+           ],
+        'ApisPath' => 'app/api/',
+        'ControllersPath' => 'app/controllers/',
+        'ModelsPath' => 'app/models/',
+        'AllowedModels' => ['WhitelistedModel']
+      }
+    )
+  end
+
+  let(:api_file) { '/root/api/my_api/app/file.rb' }
+
+  before do
+    allow(Dir).to(
+      receive(:[])
+        .with('app/models/**/*.rb')
+        .and_return(%w[app/models/some_model.rb app/models/nested/global_model.rb])
+    )
+  end
+
+  context 'no violation' do
+    describe 'when disabled api' do
+      let(:disabled_api_file) do
+        '/root/apis/fake_disabled_api/app/file.rb'
+      end
+      let(:source) do
+        <<~RUBY
+          SomeModel.find(123)
+        RUBY
+      end
+
+      it 'does not add any offenses' do
+        expect_no_offenses(source, disabled_api_file)
+      end
+    end
+
+    describe 'disabled api camel case' do
+      let(:disabled_api_file) do
+        '/root/apis/fake_disabled_api_camel/app/file.rb'
+      end
+      let(:source) do
+        <<~RUBY
+          SomeModel.find(123)
+        RUBY
+      end
+
+      it 'does not add any offenses' do
+        expect_no_offenses(source, disabled_api_file)
+      end
+    end
+
+    describe 'just accessing a const' do
+      let(:source) do
+        <<~RUBY
+          a = SomeModel::SOME_CONST
+        RUBY
+      end
+
+      it 'does not add any offenses' do
+        expect_no_offenses(source, api_file)
+      end
+    end
+
+    describe 'file in app/ outside api' do
+      let(:non_api_file) { '/root/app/file.rb' }
+      let(:source) do
+        <<~RUBY
+          SomeModel.find(123)
+        RUBY
+      end
+
+      it 'does not add any offenses' do
+        expect_no_offenses(source, non_api_file)
+      end
+    end
+
+    describe 'with whitelisted model' do
+      let(:source) do
+        <<~RUBY
+          WhitelistedModel.find(123)
+        RUBY
+      end
+
+      it 'does not add any offenses' do
+        expect_no_offenses(source, api_file)
+      end
+    end
+
+    describe 'association to model in app/' do
+      let(:non_api_file) { '/root/app/models/bar.rb' }
+      let(:source) do
+        <<~RUBY
+          class Bar < ApplicationModel
+            has_one :some_model, class_name: "SomeModel", inverse_of: :bar
+          end
+        RUBY
+      end
+
+      it 'does not add any offenses' do
+        expect_no_offenses(source, non_api_file)
+      end
+    end
+
+    describe 'association to other api model' do
+      let(:source) do
+        <<~RUBY
+          class MyApi::Foo < ApplicationModel
+            has_one :bar, class_name: "SomeOtherApi::Bar", inverse_of: :foo
+          end
+        RUBY
+      end
+
+      it 'does not add any offenses' do
+        expect_no_offenses(source, api_file)
+      end
+    end
+  end
+
+  context 'violation' do
+    describe 'when access of model from api' do
+      let(:source) do
+        <<~RUBY
+          SomeModel.find(123)
+          ^^^^^^^^^^^^^^^ Direct access of model from within Rails Api.
+        RUBY
+      end
+
+      it 'adds an offense' do
+        expect_offense(source, api_file)
+      end
+    end
+
+    describe 'association to model' do
+      let(:source) do
+        <<~RUBY
+          class MyApi::Foo < ApplicationModel
+            has_one :some_model, class_name: "SomeModel", inverse_of: :foo
+                                                    ^^^^^^^^^^^^^^^^^ Direct access of model from within Rails Api.
+          end
+        RUBY
+      end
+
+      it 'adds an offense' do
+        expect_offense(source, api_file)
+      end
+    end
+
+    context 'nested model' do
+      describe 'when access of model from engine' do
+        let(:source) do
+          <<~RUBY
+            Nested::SomeModel.find(123)
+            ^^^^^^^^^^^^^^^^^^^ Direct access of model from within Rails Api.
+          RUBY
+        end
+
+        it 'adds an offense' do
+          expect_offense(source, api_file)
+        end
+      end
+
+      describe 'association to model' do
+        let(:source) do
+          <<~RUBY
+            class MyApi::FooModel < ApplicationModel
+              has_one :nested_model, class_name: "Nested::SomeModel", inverse_of: :foo
+                                                        ^^^^^^^^^^^^^^^^^^^^^ Direct access of model from within Rails Api.
+            end
+          RUBY
+        end
+
+        it 'adds an offense' do
+          expect_offense(source, api_file)
+        end
+      end
+    end
+  end
+
+  describe '#external_dependency_checksum' do
+    it 'differs based on contents of app/models dir' do
+      old_checksum = cop.external_dependency_checksum
+      allow(Dir).to(
+        receive(:[])
+          .with('app/models/**/*.rb')
+          .and_return(['app/models/nested/model.rb'])
+      )
+      new_checksum = cop.external_dependency_checksum
+      expect(new_checksum).not_to equal(old_checksum)
+    end
+  end
+end
