3b8fe7993e2b1e3abda508ce3e89bac17ee3fa23
Jean-Francis Bastien <jfbastien@petalmd.com>
2023-05-11 14:42:12 -0400

    Use base rubocop for rubocop-petal

--- lib/rubocop/petal.rb
+++ lib/rubocop/petal.rb
@@ -3,9 +3,8 @@
 require_relative 'petal/version'
 
 module RuboCop
-  module Petal # rubocop:disable Style/Documentation
+  module Petal
     class Error < StandardError; end
-    # Your code goes here...
     PROJECT_ROOT   = Pathname.new(__dir__).parent.parent.expand_path.freeze
     CONFIG_DEFAULT = PROJECT_ROOT.join('config', 'default.yml').freeze
     CONFIG         = YAML.safe_load(CONFIG_DEFAULT.read).freeze
