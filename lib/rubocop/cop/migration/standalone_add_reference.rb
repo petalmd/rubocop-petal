a776557e596168ce527af9a3d3b1389e43e2a16b
Jean-Francis Bastien <jfbastien@petalmd.com>
2023-06-19 09:46:27 -0400

    Comment

--- lib/rubocop/cop/migration/standalone_add_reference.rb
+++ lib/rubocop/cop/migration/standalone_add_reference.rb
@@ -3,19 +3,19 @@
 module RuboCop
   module Cop
     module Migration
-      # Prevent using `add_reference` and `remove_reference` outside of.
-      # a `change_table` block. `add_reference` create multiple `ALTER TABLE`
+      # Prevent using `add_reference` and `remove_reference` outside of
+      # a `change_table` block. `add_reference` create multiples `ALTER TABLE`
       # statements. Using `change_table` with `bulk: true` is more efficient.
       #
       #  # bad
       #  add_reference :products, :user, foreign_key: true
       #
-      #   # good
-      #   change_table :products, bulk: true do |t|
-      #     t.bigint :user_id, null: false
-      #     t.index :user_id
-      #     t.foreign_key :users, column: :user_id
-      #   end
+      #  # good
+      #  change_table :products, bulk: true do |t|
+      #    t.bigint :user_id, null: false
+      #    t.index :user_id
+      #    t.foreign_key :users, column: :user_id
+      #  end
       class StandaloneAddReference < Base
         MSG = 'Modifying references must be done in a change_table block.'
 
