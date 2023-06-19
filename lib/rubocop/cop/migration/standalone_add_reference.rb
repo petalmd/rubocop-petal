6cf86b961ec5d92724bb0153a374c5b4c07ae7fa
Jean-Francis Bastien <jfbastien@petalmd.com>
2023-06-19 11:42:56 -0400

    Added `Migration/StandaloneAddReference` (#54)

---
+++ lib/rubocop/cop/migration/standalone_add_reference.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+module RuboCop
+  module Cop
+    module Migration
+      # Prevent using `add_reference` and `remove_reference` outside of
+      # a `change_table` block. `add_reference` create multiples `ALTER TABLE`
+      # statements. Using `change_table` with `bulk: true` is more efficient.
+      #
+      #  # bad
+      #  add_reference :products, :user, foreign_key: true
+      #
+      #  # good
+      #  change_table :products, bulk: true do |t|
+      #    t.bigint :user_id, null: false
+      #    t.index :user_id
+      #    t.foreign_key :users, column: :user_id
+      #  end
+      class StandaloneAddReference < Base
+        MSG = 'Modifying references must be done in a change_table block.'
+
+        RESTRICT_ON_SEND = %i[add_reference add_belongs_to remove_reference remove_belongs_to].freeze
+
+        def on_send(node)
+          reference_method = node.source_range.with(end_pos: node.child_nodes.first.source_range.begin_pos - 1)
+
+          add_offense(reference_method)
+        end
+      end
+    end
+  end
+end
---
+++ spec/rubocop/cop/migration/standalone_add_reference_spec.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+RSpec.describe RuboCop::Cop::Migration::StandaloneAddReference, :config do
+  it 'registers an offense when modifing a references outside a change_table' do
+    expect_offense(<<~RUBY)
+      add_reference :products, :user
+      ^^^^^^^^^^^^^ Modifying references must be done in a change_table block.
+    RUBY
+
+    expect_offense(<<~RUBY)
+      add_reference :products, :user, index: true
+      ^^^^^^^^^^^^^ Modifying references must be done in a change_table block.
+    RUBY
+
+    expect_offense(<<~RUBY)
+      add_belongs_to :products, :user
+      ^^^^^^^^^^^^^^ Modifying references must be done in a change_table block.
+    RUBY
+
+    expect_offense(<<~RUBY)
+      remove_reference :products, :user
+      ^^^^^^^^^^^^^^^^ Modifying references must be done in a change_table block.
+    RUBY
+
+    expect_offense(<<~RUBY)
+      remove_belongs_to :products, :user
+      ^^^^^^^^^^^^^^^^^ Modifying references must be done in a change_table block.
+    RUBY
+  end
+
+  it 'does not register an offense when not calling modifying references methods' do
+    expect_no_offenses(<<~RUBY)
+      add_index :users, :user_id
+    RUBY
+  end
+end
