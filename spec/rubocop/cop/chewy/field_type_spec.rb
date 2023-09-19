51743d769455b04b1b21d3a566f30272a0c01e3c
Jean-Francis Bastien <jfbastien@petalmd.com>
2023-09-19 14:38:49 -0400

    Added Chewy/FieldType

---
+++ spec/rubocop/cop/chewy/field_type_spec.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+RSpec.describe RuboCop::Cop::Chewy::FieldType, :config do
+  it 'registers an offense when no specifying a field' do
+    expect_offense(<<~RUBY)
+      field :name
+      ^^^^^^^^^^^ Specify a `type` for Chewy field.
+    RUBY
+
+    expect_offense(<<~RUBY)
+      field :name, value: 'text'
+      ^^^^^^^^^^^^^^^^^^^^^^^^^^ Specify a `type` for Chewy field.
+    RUBY
+
+    expect_offense(<<~RUBY)
+      field :name, value: lambda { |a| a.name.upcase }
+      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Specify a `type` for Chewy field.
+    RUBY
+
+    expect_offense(<<~RUBY)
+      defined_type User do
+        field :name
+        ^^^^^^^^^^^ Specify a `type` for Chewy field.
+      end
+    RUBY
+
+    expect_offense(<<~RUBY)
+      field :name, :email
+      ^^^^^^^^^^^^^^^^^^^ Specify a `type` for Chewy field.
+    RUBY
+  end
+
+  it 'does not register an offense when a type is specified' do
+    expect_no_offenses(<<~RUBY)
+      field :name, type: 'text'
+    RUBY
+
+    expect_no_offenses(<<~RUBY)
+      field :name, type: :text
+    RUBY
+
+    expect_no_offenses(<<~RUBY)
+      field :name, :email, type: 'text'
+    RUBY
+  end
+end
