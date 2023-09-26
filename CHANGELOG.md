43c0553fd94a02ac58b2d0bf00c5085122c0ab44
Jean-Francis Bastien <jfbastien@petalmd.com>
2023-09-26 09:48:32 -0400

    Added Rails/DestroyAllBang (#65)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,7 @@
 
 # main
 
+* Added cop `Rails/DestroyAllBang`(Fix [#42](https://github.com/petalmd/rubocop-petal/issues/42)). ([#65](https://github.com/petalmd/rubocop-petal/pull/65))
 * Added cop `Chewy/FieldType`. ([#64](https://github.com/petalmd/rubocop-petal/pull/64))
 * Fix `Migration/ChangeTableReferences` offense location (Fix [#61](https://github.com/petalmd/rubocop-petal/issues/61)) ([#62](https://github.com/petalmd/rubocop-petal/pull/62))
 * Added cop `Sidekiq/NoEarlyNilReturn` ([#58](https://github.com/petalmd/rubocop-petal/pull/58))
---
+++ spec/rubocop/cop/rails/destroy_all_bang_spec.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+RSpec.describe RuboCop::Cop::Rails::DestroyAllBang, :config do
+  it 'registers an offense when using `destroy_all`' do
+    expect_offense(<<~RUBY)
+      User.destroy_all
+           ^^^^^^^^^^^ Use `each(&:destroy!)` instead of `destroy_all`.
+    RUBY
+
+    expect_correction(<<~RUBY)
+      User.each(&:destroy!)
+    RUBY
+
+    expect_offense(<<~RUBY)
+      User.where(desactivated: true).destroy_all
+                                     ^^^^^^^^^^^ Use `each(&:destroy!)` instead of `destroy_all`.
+    RUBY
+
+    expect_correction(<<~RUBY)
+      User.where(desactivated: true).each(&:destroy!)
+    RUBY
+
+    expect_offense(<<~RUBY)
+      User
+        .where(desactivated: true)
+        .destroy_all
+         ^^^^^^^^^^^ Use `each(&:destroy!)` instead of `destroy_all`.
+    RUBY
+
+    expect_correction(<<~RUBY)
+      User
+        .where(desactivated: true)
+        .each(&:destroy!)
+    RUBY
+  end
+
+  it 'does not register an offense when using `#each(&:destroy!)`' do
+    expect_no_offenses(<<~RUBY)
+      User.each(&:destroy!)
+    RUBY
+  end
+end
