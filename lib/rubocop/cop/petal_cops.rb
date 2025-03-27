f12da281ad258df901f181dee0f43159eeb9aea1
Jean-Francis Bastien <jfbastien@petalmd.com>
2025-03-27 10:58:19 -0400

    init new cop

--- lib/rubocop/cop/petal_cops.rb
+++ lib/rubocop/cop/petal_cops.rb
@@ -6,3 +6,4 @@ require 'rubocop-rails'
 # Auto-require all cops under `rubocop/cop/**/*.rb`
 cops_glob = File.join(__dir__, '**', '*.rb')
 Dir[cops_glob].sort.each { |cop| require(cop) }
+require_relative 'rspec/sidekiq_perform_matcher'
