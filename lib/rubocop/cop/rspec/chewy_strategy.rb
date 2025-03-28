25a8ce4ea3664d2122fe4d1a494406c122857249
Jean-Francis Bastien <jfbastien@petalmd.com>
2025-03-28 08:46:43 -0400

    Add RSpec/ChewyStrategy cop (#93)

---
+++ lib/rubocop/cop/rspec/chewy_strategy.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+module RuboCop
+  module Cop
+    module RSpec
+      # Prevent to change the Chewy strategy in tests.
+      # Setting the strategy to atomic may try to import unnecessary data
+      # for the test which result to a slower test suite.
+      #
+      # @example
+      #   # bad
+      #   let(:user) { Chewy.strategy(:atomic) { create(:user) } }
+      #
+      #   # good
+      #   let(:user) { create(:user) }
+      #   before { UserIndex.import! user }
+      #
+      class ChewyStrategy < Base
+        MSG = 'Do not use Chewy.strategy in tests. Import data explicitly instead.'
+        RESTRICT_ON_SEND = %i[strategy].freeze
+
+        # @!method chewy_strategy?(node)
+        def_node_matcher :chewy_strategy?, <<~PATTERN
+          (send (const nil? :Chewy) :strategy ...)
+        PATTERN
+
+        def on_send(node)
+          return unless chewy_strategy?(node)
+
+          add_offense(node)
+        end
+        alias on_csend on_send
+      end
+    end
+  end
+end
---
+++ spec/rubocop/cop/rspec/chewy_strategy_spec.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+
+RSpec.describe RuboCop::Cop::RSpec::ChewyStrategy, :config do
+  let(:config) { RuboCop::Config.new }
+
+  it 'registers an offense when using Chewy.strategy' do
+    expect_offense(<<~RUBY)
+      let(:user) { Chewy.strategy(:atomic) { create(:user) } }
+                   ^^^^^^^^^^^^^^^^^^^^^^^ RSpec/ChewyStrategy: Do not use Chewy.strategy in tests. Import data explicitly instead.
+    RUBY
+  end
+
+  it 'registers an offense for multiline Chewy.strategy usage' do
+    expect_offense(<<~RUBY)
+      let(:user) do
+        Chewy.strategy(:urgent) do
+        ^^^^^^^^^^^^^^^^^^^^^^^ RSpec/ChewyStrategy: Do not use Chewy.strategy in tests. Import data explicitly instead.
+          create(:user)
+        end
+      end
+    RUBY
+  end
+
+  it 'registers an offense when assigning Chewy.strategy to a variable' do
+    expect_offense(<<~RUBY)
+      before do
+        result = Chewy.strategy(:bypass) { create(:user) }
+                 ^^^^^^^^^^^^^^^^^^^^^^^ RSpec/ChewyStrategy: Do not use Chewy.strategy in tests. Import data explicitly instead.
+      end
+    RUBY
+  end
+
+  it 'registers an offense when using Chewy.strategy in a before block' do
+    expect_offense(<<~RUBY)
+      before do
+        Chewy.strategy(:zero) { User.create!(name: 'Test') }
+        ^^^^^^^^^^^^^^^^^^^^^ RSpec/ChewyStrategy: Do not use Chewy.strategy in tests. Import data explicitly instead.
+      end
+    RUBY
+  end
+
+  it 'registers an offense for different strategy types' do
+    expect_offense(<<~RUBY)
+      it 'creates a user' do
+        Chewy.strategy(:bypass) { create(:user) }
+        ^^^^^^^^^^^^^^^^^^^^^^^ RSpec/ChewyStrategy: Do not use Chewy.strategy in tests. Import data explicitly instead.
+      end
+    RUBY
+  end
+
+  it 'registers an offense when using Chewy.strategy in a helper method' do
+    expect_offense(<<~RUBY)
+      def create_user
+        Chewy.strategy(:urgent) { create(:user) }
+        ^^^^^^^^^^^^^^^^^^^^^^^ RSpec/ChewyStrategy: Do not use Chewy.strategy in tests. Import data explicitly instead.
+      end
+    RUBY
+  end
+end
